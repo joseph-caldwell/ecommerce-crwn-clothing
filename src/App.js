33041686a9855e54031301e3776660dfc8cc3822
joseph-caldwell <caldwelljoey36@gmail.com>
2021-10-28 16:32:28 -0500

    added withRoute()r to menu-item
    'added withRoute()r to menu-item'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Homepage from './pages/homepage/Homepage.component'
 import './App.css';
+import { Switch, Route } from 'react-router';
+
+
+const HatsPage =()=> (
+  <div>
+    <h1>HATS PAGE</h1>
+
+  </div>
+);
 
 export default function App() {
   return (
-   <Homepage />
-  );
+    <div>
+    <Switch>
+      <Route exact path='/' component='Homepage' />
+      <Route exact path='/hats' component='Hatspage' />
+    </Switch>
+    
+    </div>
+);
 }
 
+//  <Homepage />
