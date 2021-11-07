e36019c78c7bf15f2cb6d4bcef048de35339a842
joseph-caldwell <caldwelljoey36@gmail.com>
2021-11-07 00:50:44 -0500

    added collection-preview,shopPage, & collection-item

--- src/components/Collection-Preview-Component/Collection-Preview.Component.jsx
+++ src/components/Collection-Preview-Component/Collection-Preview.Component.jsx
@@ -8,11 +8,11 @@ const CollectionPreview = ({ title,items,id }) => {
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-            {
-                items.filter((item,idx)=>(
+            
+                {items.filter(item=>(
                     <div key={item.id}>{ item.name}</div>
-                ))
-            }
+                ))}
+            
             </div>
         </div>
     );
